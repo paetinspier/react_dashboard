479e073b9a34bdc1a0a353bf5f600003c3adff99
paetinspier <paetin.nelson@gmail.com>
2022-11-02 11:06:46 -0700

    initial

---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Inject, SparklineTooltip, SparklineComponent } from '@syncfusion/ej2-react-charts';
+
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName='x'
+      yName='yval'
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        format: '${x} : data ${y}',
+        trackLineSettings: {
+          visible: true,
+        }
+      }}
+    >
+      <Inject services={[SparklineTooltip]}/>
+    </SparklineComponent>
+  );
+}
+
+export default SparkLine
---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,63 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setisClicked] = useState(initialState);
+  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('Light');
+  const [themeSettings, setThemeSettings] = useState(false);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem('themeMode', e.target.value);
+  };
+
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem('colorMode', color);
+  };
+
+  const handleClick = (clicked) => {
+    setisClicked({ ...initialState, [clicked]: true });
+  };
+
+
+
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setisClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+        currentColor,
+        currentMode,
+        setColor,
+        setMode,
+        themeSettings,
+        setThemeSettings,
+        setCurrentMode,
+        setCurrentColor
+
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
